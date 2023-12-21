a64d8688d6c04d419a16c8b9c6103d2dd739991e
Shubham Kumar <unresolved.shubham@gmail.com>
2023-12-21 21:40:06 +0530

    Updated the header and license data as per @riscy advice

--- jmespath.el
+++ jmespath.el
@@ -1,6 +1,17 @@
-;;; jmespath.el --- JSON query using jmespath -*- lexical-binding: t; -*-
-;;
+;;; jmespath.el --- Query JSON using jmespath -*- lexical-binding: t; -*-
+
 ;; Copyright (C) 2023 Shubham Kumar
+
+;; Author: Shubham Kumar <unresolved.shubham@gmail.com>
+;; Maintainer: Shubham Kumar <unresolved.shubham@gmail.com>
+;; Created: December 17, 2023
+;; Modified: December 17, 2023
+;; Version: 0.0.1
+;; Keywords: json, data, languages, tools
+;; Homepage: https://github.com/unresolvedcold/jmespath
+;; Package-Requires: ((emacs "24.3"))
+
+;; This file is not part of GNU Emacs.
 
 ;; Permission is hereby granted, free of charge, to any person obtaining a copy
 ;; of this software and associated documentation files (the "Software"), to deal
@@ -19,37 +30,16 @@
 ;; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 ;; OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 ;; SOFTWARE.
-;;
-;; Author: Shubham Kumar <unresolved.shubham@gmail.com>
-;; Maintainer: Shubham Kumar <unresolved.shubham@gmail.com>
-;; Created: December 17, 2023
-;; Modified: December 17, 2023
-;; Version: 0.0.1
-;; Keywords: abbrev bib c calendar comm convenience data docs emulations extensions faces files frames games hardware help hypermedia i18n internal languages lisp local maint mail matching mouse multimedia news outlines processes terminals tex tools unix vc wp
-;; Homepage: https://github.com/unresolvedcold/jmespath
-;; Package-Requires: ((emacs "24.3") (json-mode "1.8.0"))
-;;
-;; This file is not part of GNU Emacs.
-;;
-;;; Commentary:
-;; Library to query json using JMESPath.
-;;
-;;;  Description:
-;;
-;; A simple jmespath parser that used jp underneath to query the json file.
-;; All you need to do is run jmespath-query function and enter your jmespath query.
-;; The queried result will be displayed in the *JMESPath Result* buffer
-;; Note: This requires jp library to be installed.
-;; More on jp - https://github.com/jmespath/jp
-;;
-;;; Installation:
-;;
-;; This needs jp installed locally in your system.
-;; This utility is dependent on json-mode, please instal that too.
-;;
-;;; Code:
 
-(require 'json-mode)
+;;; Commentary:
+
+;; A simple jmespath parser that uses jp underneath to query the json file.  All
+;; you need to do is run the `jmespath-query' function and enter your jmespath
+;; query.  The queried result will be displayed in the *JMESPath Result* buffer
+
+;; This package depends on the jp utility: https://github.com/jmespath/jp
+
+;;; Code:
 
 (defun jmespath-query (query)
   "Execute a JMESPath QUERY on the current buffer."
@@ -63,7 +53,6 @@
         (erase-buffer)
         (insert (format "JMESPath Query: %s\n\n" query))
         (insert result)
-        (jsonc-mode)
         (pop-to-buffer result-buffer)))))
 
 (provide 'jmespath)
