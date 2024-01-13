9d688c0620dcde91bdee668cad97293fdf50c13b
Shubham Kumar <unresolved.shubham@gmail.com>
2024-01-14 03:23:50 +0530

    Minor changes

--- jmespath.el
+++ jmespath.el
@@ -34,8 +34,9 @@
 ;;; Commentary:
 
 ;; A simple jmespath parser that uses jp underneath to query the json file.  All
-;; you need to do is run the `jmespath-query' function and enter your jmespath
+;; you need to do is run the `jmespath-query-and-show' function and enter your jmespath
 ;; query.  The queried result will be displayed in the *JMESPath Result* buffer
+;; To run the query on a file, run `C-u M-x jmespath-query-and-show`.
 
 ;; This package depends on the jp utility: https://github.com/jmespath/jp
 
